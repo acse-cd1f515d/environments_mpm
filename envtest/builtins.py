2b65419498d60214aad9b7ef98d68ee5e7fdd4ed
acse-cd1f515d <jingyang.min22@imperial.ac.uk>
2022-10-08 20:39:39 +0100

    updates

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,23 @@
 import numpy as np
+from scipy.ndimage import gaussian_filter
+from scipy import misc
+import pandas as pd
 
 
-__all__ = ['rand_array']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'pandas_show_example']
 
 
 def rand_array(shape):
     return np.random.rand(*shape)
+
+
+def smooth_image(a, sigma=1):
+    return gaussian_filter(a, sigma=sigma)
+
+
+def my_mat_solve(A, b):
+    return A.inv()*b
+
+
+def pandas_show_example():
+    return pd.Series([1, 3, 5, np.nan, 6, 8])
---
+++ scripts/show_series.py
@@ -0,0 +1,3 @@
+from envtest import pandas_show_example
+
+print(pandas_show_example())
